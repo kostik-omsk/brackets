4000a7cd09efb32e060a89dbfaa03074a2f0799b
sidorenko konstantin <kosiak5587@gmail.com>
2023-01-18 12:57:54 +0600

    feat: add ready function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  let stack = [];
+  for (let i = 0; i < str.length; i++) {
+    for (let item of bracketsConfig) {
+      if (
+        str[i] == item[1] &&
+        stack.length &&
+        stack[stack.length - 1] == item[0]
+      ) {
+        stack.pop();
+      } else if (str[i] == item[0]) {
+        stack.push(item[0]);
+      } else if (str[i] == item[1]) {
+        return false;
+      }
+    }
+  }
+  return stack.length == 0;
+};
